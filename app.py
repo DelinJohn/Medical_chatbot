893d4954fbc46e6257f7390abfd2cacd465bb5c1
delin <johndelinshaji@gmail.com>
2024-10-24 16:54:01 +0530

    correctiong some deprected code

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain_groq import ChatGroq
 from typing import Literal
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.utilities import SQLDatabase
-from langchain_core.pydantic_v1 import BaseModel,Field
+from pydantic import BaseModel,Field
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_ollama import OllamaEmbeddings
